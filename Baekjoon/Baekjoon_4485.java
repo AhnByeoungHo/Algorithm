e0e5b9001795fab05e29c66d327874dc5a075a91
anjimmy <anjimmy@DESKTOP-BSUNE7J>
2020-04-10 17:10:54 +0900

    Baekjoon 4485

--- Baekjoon/Baekjoon_4485.java
+++ Baekjoon/Baekjoon_4485.java
@@ -1,230 +1,77 @@
-package bak20200211;
+package Baekjoon;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-/*
- * @author AhnByeoungHo
- * @since 2020. 2. 11.
- * @see https://www.acmicpc.net/problem/4485
- * @mem
- * @time
- * @caution DFS Not solved time over
- * 
- * */
 
 public class Baekjoon_4485 {
 
-	static int[][] map;
-	private static boolean[][] visit;
+	private static int[][] map;
 	private static int N;
 	private static int ans;
+	private static int[] dx = {-1,1,0,0};
+	private static int[] dy = {0,0,-1,1};
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int tc = 1;
-		while(true) {
+		while(true){
 			N = Integer.parseInt(br.readLine());
 			if(N==0) break;
 			map = new int[N][N];
-
-			for(int i=0;i<N;i++) {
+			for(int i=0;i<N;i++){
 				String str = br.readLine();
-				for(int j=0;j<N;j++) {
+				for(int j=0;j<N;j++){
 					map[i][j] = str.charAt(j*2)-'0';
 				}
 			}
+			ans = 0;
+			int[][] D = new int[N][N];
+			for(int i=0;i<N;i++)
+				Arrays.fill(D[i], Integer.MAX_VALUE);
+			boolean[][] check = new boolean[N][N];
 
-			ans = Integer.MAX_VALUE;
+			//dijkstra 시작점이 a정점이라면 D[a] = 0
+			D[0][0] = map[0][0];
+			for(int i = 0; i < N; i++) {
+				for( int j = 0; j < N; j++ ) {
+					int min = Integer.MAX_VALUE;//가장 작은 값을 기억할 변수
+					int curx = -1; //그 위치를 기억할 변수
+					int cury = -1;
+					//아직 처리하지 않았으면서, 가장 짧은 거리라면
+					for (int x = 0; x < N; x++) {
+						for (int k = 0; k < N; k++) {
+							if(!check[x][k] && min >= D[x][k]) {
+								min = D[x][k];
+								curx = x;
+								cury = k;
+							}
+						}
+					}
+					
+					//연결이 없는 경우 끝.
+					if(curx == -1 || cury == -1)
+						break;
 
-			visit = new boolean[N][N];
-			visit[0][0] = true;
-			visit[0][1] = true;
-			dfs(0,1,2,0);
+					for(int dir=0;dir<4;dir++){
+						int nx = curx + dx[dir];
+						int ny = cury + dy[dir];
+						if(nx<0||ny<0||nx>=N||ny>=N) continue;
+						//if(check[nx][ny]) continue;
+						 
+						if((D[curx][cury] + map[nx][ny])>D[nx][ny]) continue;
+						
+						D[nx][ny] = D[curx][cury] + map[nx][ny];
+					}
+					check[curx][cury] = true;
 
-			
-			visit = new boolean[N][N];
-			visit[0][0] = true;
-			visit[1][0] = true;
-			dfs(1,0,1,0);
-			System.out.println("Problem"+tc+": "+ (ans+map[0][0]+map[N-1][N-1]));
-			tc++;
+				}
+			}	
+			System.out.println("Problem "+(tc++)+": " +D[N-1][N-1]);
 		}
 	}
-	public static void dfs(int x, int y, int dir, int cnt) {
-		//System.out.println(x+" "+y);
-		if((x==N-1&&y==N-2)) {
-			cnt += map[N-1][N-2];
-			if(cnt<ans) 
-				ans = cnt;
-			return;
-		}
-		if((x==N-2&&y==N-1)) {
-			cnt += map[N-2][N-1];
-			if(cnt<ans) 
-				ans = cnt;
-			return;
-		}
-		if(x<0||y<0||x>=N||y>=N)
-			return;
-		else {
-			int up= Integer.MAX_VALUE,down= Integer.MAX_VALUE,
-					left= Integer.MAX_VALUE,right = Integer.MAX_VALUE;
-			if(x>0)
-				up = map[x-1][y];
-			if(x<N-1)
-				down = map[x+1][y];
-			if(y>0)
-				left = map[x][y-1];
-			if(y<N-1)
-				right = map[x][y+1];
-
-			if(dir==1) { 		//상에서온것
-				int min = Integer.min(down, left);
-				min = Integer.min(min,right);
-
-				if(x<N-1) {					//아래로내려가는것 방향 위에서 온거라고 표시
-					if(map[x+1][y]==min) {
-						if(!visit[x+1][y]) {
-							visit[x+1][y] = true;
-							dfs(x+1,y,1,cnt+map[x][y]);
-							visit[x+1][y] = false;
-						}
-
-					}
-				}
-				if(y>1) {					//왼쪽으로 가는것 오른쪽으로 온거라고 표시
-					if(map[x][y-1]==min) {
-						if(!visit[x][y-1]) {
-							visit[x][y-1] = true;
-							dfs(x,y-1,4,cnt+map[x][y]);
-							visit[x][y-1] = false;
-						}
-
-					}
-				}
-				if(y<N-1) {					//오른쪽으로가는것 왼쪽에서 온거라고 표시
-					if(map[x][y+1]==min) {
-						if(!visit[x][y+1]) {
-							visit[x][y+1] = true;
-							dfs(x,y+1,2,cnt+map[x][y]);
-							visit[x][y+1] = false;
-						}
-
-					}
-				}
-
-
-			}
-			else if(dir==2) {	//왼에서온것
-				int min = Integer.min(down, up);
-				min = Integer.min(min,right);
-
-				if(x>0) {
-					if(map[x-1][y]==min) {	//위로올라가는것 아래에서 온거라고 표시
-						if(!visit[x-1][y]) {
-							visit[x-1][y] = true;
-							dfs(x-1,y,3,cnt+map[x][y]);
-							visit[x-1][y] = false;
-						}
-
-					}
-				}
-				if(x<N-1) {					//아래로내려가는것 방향 위에서 온거라고 표시
-					if(map[x+1][y]==min) {
-						if(!visit[x+1][y]) {
-							visit[x+1][y] = true;
-							dfs(x+1,y,1,cnt+map[x][y]);
-							visit[x+1][y] = false;
-						}
-
-					}
-				}
-				if(y<N-1) {					//오른쪽으로가는것 왼쪽에서 온거라고 표시
-					if(map[x][y+1]==min) {
-						if(!visit[x][y+1]) {
-							visit[x][y+1] = true;
-							dfs(x,y+1,2,cnt+map[x][y]);
-							visit[x][y+1] = false;
-						}
-
-					}
-				}
-			}
-			else if(dir==3) {	//하에서온것
-				int min = Integer.min(left, up);
-				min = Integer.min(min,right);
-
-				if(y<N-1) {					//오른쪽으로가는것 왼쪽에서 온거라고 표시
-					if(map[x][y+1]==min) {
-						if(!visit[x][y+1]) {
-							visit[x][y+1] = true;
-							dfs(x,y+1,2,cnt+map[x][y]);
-							visit[x][y+1] = false;
-						}
-
-					}
-				}
-				if(x>0) {
-					if(map[x-1][y]==min) {	//위로올라가는것 아래에서 온거라고 표시
-						if(!visit[x-1][y]) {
-							visit[x-1][y] = true;
-							dfs(x-1,y,3,cnt+map[x][y]);
-							visit[x-1][y] = false;
-						}
-
-					}
-				}
-				if(y>0) {					//왼쪽으로 가는것 오른쪽으로 온거라고 표시
-					if(map[x][y-1]==min) {
-						if(!visit[x][y-1]) {
-							visit[x][y-1] = true;
-							dfs(x,y-1,4,cnt+map[x][y]);
-							visit[x][y-1] = false;
-						}
-
-					}
-				}
-			}
-			else if(dir==4) {	//오에서온것
-				int min = Integer.min(down, up);
-				min = Integer.min(min,left);
-
-				if(y>1) {					//왼쪽으로 가는것 오른쪽으로 온거라고 표시
-					if(map[x][y-1]==min) {
-						if(!visit[x][y-1]) {
-							visit[x][y-1] = true;
-							dfs(x,y-1,4,cnt+map[x][y]);
-							visit[x][y-1] = false;
-						}
-
-					}
-				}
-				if(x>1) {
-					if(map[x-1][y]==min) {	//위로올라가는것 아래에서 온거라고 표시
-						if(!visit[x-1][y]) {
-							visit[x-1][y] = true;
-							dfs(x-1,y,3,cnt+map[x][y]);
-							visit[x-1][y] = false;
-						}
-
-					}
-				}
-				if(x<N-1) {					//아래로내려가는것 방향 위에서 온거라고 표시
-					if(map[x+1][y]==min) {
-						if(!visit[x+1][y]) {
-							visit[x+1][y] = true;
-							dfs(x+1,y,1,cnt+map[x][y]);
-							visit[x+1][y] = false;
-						}
-
-					}
-				}
-
-			}
-		}
-	}
-
 }
